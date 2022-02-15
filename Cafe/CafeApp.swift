e3aec89fe76101b55487f1104eabc2b4b918f62f
Yujin Jeong <jinietree20@gmail.com>
2022-02-15 16:12:32 +0900

    Initial Commit

---
+++ Cafe/CafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeApp.swift
+//  Cafe
+//
+//  Created by UAPMobile on 2022/02/15.
+//
+
+import SwiftUI
+
+@main
+struct CafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
